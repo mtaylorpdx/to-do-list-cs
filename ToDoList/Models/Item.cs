6b7b078205553f67c23a89ce14a5789ece5b150b
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-17 00:28:04 -0700

    follow along

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,61 +1,8 @@
-using System.Collections.Generic;
-using MySql.Data.MySqlClient;
-
 namespace ToDoList.Models
 {
   public class Item
   {
+    public int ItemId { get; set; }
     public string Description { get; set; }
-    public int Id { get; }
-    private static List<Item> _instances = new List<Item> {};
-
-    public Item (string description)
-    {
-      Description = description;
-    }
-    public Item(string description, int id)
-    {
-        Description = description;
-        Id = id;
-    }
-    
-    public static List<Item> GetAll()
-    {
-        List<Item> allItems = new List<Item> { };
-        MySqlConnection conn = DB.Connection();
-        conn.Open();
-        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-        cmd.CommandText = @"SELECT * FROM items;";
-        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-        while (rdr.Read())
-        {
-            int itemId = rdr.GetInt32(0);
-            string itemDescription = rdr.GetString(1);
-            Item newItem = new Item(itemDescription, itemId);
-            allItems.Add(newItem);
-        }
-        conn.Close();
-        if (conn != null)
-        {
-            conn.Dispose();
-        }
-        return allItems;
-    }
-
-   public static void ClearAll()
-   {
-     MySqlConnection conn = DB.Connection();
-     conn.Open();
-     MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-     cmd.CommandText = @"DELETE FROM items;";
-     cmd.ExecuteNonQuery();
-  }
-
-    public static Item Find(int searchId)
-    {
-      // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with database.
-      Item placeholderItem = new Item("placeholder item");
-      return placeholderItem;
-    }
   }
 }
